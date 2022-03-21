0ba4ce53ff2a3697e11dcb5ae456de6109602e9c
Misael Kellermann <77032957+kellermannhub@users.noreply.github.com>
2022-03-21 17:43:44 -0300

    Update paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -7,10 +7,3 @@ PARENT_DIRECTORY = None
 def set_parent_directory(file):
     global PARENT_DIRECTORY
 
-    PARENT_DIRECTORY = pathlib.Path(file).parent.resolve()
-    print("=" * 30)
-    print(PARENT_DIRECTORY)
-    print("=" * 30)
-
-
-print(PARENT_DIRECTORY)
